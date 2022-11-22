0d15db2e69a2c70122361241c17eb4ee43ffe53c
Hayet Fellah <hayet.fellah145@gmail.com>
2022-11-22 23:06:08 +0100

    Add Header and Routes to App

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import { useDispatch } from 'react-redux';
+import { useEffect } from 'react';
+import { Route, Routes } from 'react-router-dom';
+import { fetchCountriesData } from './redux/countries/countries';
+import Header from './components/Header';
+import HomePage from './components/HomePage';
+import CountryInfo from './components/CountryInfo';
 
 function App() {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchCountriesData());
+  }, [dispatch]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <main>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/countries/:countryName" element={<CountryInfo />} />
+        </Routes>
+      </main>
     </div>
   );
 }
