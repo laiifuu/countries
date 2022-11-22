4a23c9eda0e51d7ff2dce682f7dfdc77dfc3f830
Hayet Fellah <hayet.fellah145@gmail.com>
2022-11-22 23:28:14 +0100

    Create HomePage component

---
+++ src/components/HomePage.js
@@ -0,0 +1,13 @@
+import ContinentPicker from './ContinentPicker';
+import Map from './Map';
+import Countries from './Countries';
+
+const HomePage = () => (
+  <>
+    <ContinentPicker />
+    <Map />
+    <Countries />
+  </>
+);
+
+export default HomePage;
