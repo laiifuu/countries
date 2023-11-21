ca2b5aaa526eeb89180135aaa167437b711e1ec8
laiifuu <hayet.fellah145@gmail.com>
2023-11-21 17:19:44 +0100

    Add all components and routes and style the app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
 import { useDispatch } from 'react-redux';
 import { useEffect } from 'react';
-// import { Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import { fetchCountriesData } from './redux/countries/countries';
 import Header from './components/Header';
-// import HomePage from './components/HomePage';
-// import About from './components/About';
-// import CountryInfo from './components/CountryInfo';
+import HomePage from './components/HomePage';
+import About from './components/About';
+import CountryInfo from './components/CountryInfo';
 import Footer from './components/Footer';
 
 function App() {
@@ -18,13 +18,13 @@ function App() {
   return (
     <div className="App">
       <Header />
-      {/* <main>
+      <main>
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/countries/:countryName" element={<CountryInfo />} />
           <Route path="/About" element={<About />} />
         </Routes>
-      </main> */}
+      </main>
       <Footer />
     </div>
   );
